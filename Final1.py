8afa9c54926857d235749e0c71ebbc62c87c4d33
Matt Mauzy <mrmauzy13@gmail.com>
2018-12-02 20:01:57 -0600

    Update Final1.py

--- Final1.py
+++ Final1.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 #
-# GrovePi Example for using the Grove Light Sensor and the LED together to turn the LED On and OFF if the background light is greater than a threshold.
+# GrovePi Example for using the Grove Light Sensor and the LED together to turn the LED On and OFF if the
+#  background light is greater than a threshold.
 # Modules:
 # 	http://www.seeedstudio.com/wiki/Grove_-_Light_Sensor
 # 	http://www.seeedstudio.com/wiki/Grove_-_LED_Socket_Kit
 #
-# The GrovePi connects the Raspberry Pi and Grove sensors.  You can learn more about GrovePi here:  http://www.dexterindustries.com/GrovePi
+# The GrovePi connects the Raspberry Pi and Grove sensors.  You can learn more about GrovePi here:
+#  http://www.dexterindustries.com/GrovePi
 #
 # Have a question about this example?  Ask on the forums here:  http://forum.dexterindustries.com/c/grovepi
 #
@@ -42,12 +44,13 @@ from math import isnan
 # SIG,NC,VCC,GND
 light_sensor = 0
 
-# Temp / Hum Sensor is attached to port 7
+# Temp / humidity Sensor is attached to port 7
 dht_sensor_port = 7
 dht_sensor_type = 0
 
 # Connect the LED to digital port D4
 # SIG,NC,VCC,GND
+# Blue, Green and Red LED's
 BLed = 4
 GLed = 5
 RLed = 6
@@ -56,10 +59,10 @@ RLed = 6
 # Turn on LED once sensor exceeds threshold resistance
 threshold = 15
 
-grovepi.pinMode(light_sensor,"INPUT")
-grovepi.pinMode(BLed,"OUTPUT")
-grovepi.pinMode(GLed,"OUTPUT")
-grovepi.pinMode(RLed,"OUTPUT")
+grovepi.pinMode(light_sensor, "INPUT")
+grovepi.pinMode(BLed, "OUTPUT")
+grovepi.pinMode(GLed, "OUTPUT")
+grovepi.pinMode(RLed, "OUTPUT")
 
 outputData = []
 
@@ -67,6 +70,13 @@ outputData = []
 sleep(.1)
 
 while True:
+
+    def lights():
+        grovepi.digitalWrite(GLed, 0)
+        grovepi.digitalWrite(BLed, 0)
+        grovepi.digitalWrite(RLed, 0)
+
+
     try:
         sleep(0.5)
         # Get sensor value
@@ -76,51 +86,47 @@ while True:
         if sensor_value <= 0: # stops dividing by 0 errors
             resistance = 0
         else:
-            resistance = (float)(1023 - sensor_value) * 10 / sensor_value
+            resistance = float(1023 - sensor_value) * 10 / sensor_value
 
-	# If Light is on, records data
+        # If Light is on, records data
         if resistance > threshold:
             # Send HIGH to switch off LED
-            grovepi.digitalWrite(RLed,0)
-            grovepi.digitalWrite(GLed,0)
-            grovepi.digitalWrite(BLed,0)
-	# If light is off, turns off and stop recording
+            lights()
+        # If light is off, turns off and stop recording
         else:
-            [temp, hum] = dht(dht_sensor_port, dht_sensor_type)
-            fahr = (temp * 9/5) +32 # celcius to Fahr
-            print(fahr, hum)
-            t = fahr
-            h = hum
-            # Send LOW to switch on LED depening on the Temp and Hum
-            if fahr > 95:
-                grovepi.digitalWrite(RLed,1)
-            elif (fahr > 60 and fahr < 85) and (hum < 80):
-                grovepi.digitalWrite(GLed,1)
-            elif (fahr > 85 and fahr < 95) and (hum < 80):
-                grovepi.digitalWrite(BLed,1)
-            elif (hum > 80):
-                grovepi.digitalWrite(GLed,1)
-                grovepi.digitalWrite(BLed,1)
+            [temp, humidity] = dht(dht_sensor_port, dht_sensor_type)
+            # celsius to fahrenheit conversion
+            fahrenheit = (temp * 9/5) + 32
+            print(fahrenheit, humidity)
+            t = fahrenheit
+            h = humidity
+            # Send LOW to switch on LED depending on the Temp and humidity
+            if fahrenheit > 95:
+                grovepi.digitalWrite(RLed, 1)
+            elif (60 > fahrenheit < 85) and (humidity < 80):
+                grovepi.digitalWrite(GLed, 1)
+            elif (85 > fahrenheit < 95) and (humidity < 80):
+                grovepi.digitalWrite(BLed, 1)
+            elif humidity > 80:
+                grovepi.digitalWrite(GLed, 1)
+                grovepi.digitalWrite(BLed, 1)
             else:
                 print("No Readings!")
-            #Sends data to the global array
+            # Sends data to the global array
             outputData.append([t, h])
 
-        #Used for testing purposes
-        #print("sensor_value = %d resistance = %.2f" %(sensor_value,  resistance))
-	# Sleeps for 1800 seconds, or 30 minutes
+        # Sleeps for 1800 seconds, or 30 minutes
         sleep(1800)
 
     except IOError:
-        print ("Error")
+        print("Error")
 
     except KeyboardInterrupt:
         print("Program Terminated by User!!!") # Takes keyboard interrupt and ends
-        #Turns off lights ones program stops
-        grovepi.digitalWrite(GLed,0)
-        grovepi.digitalWrite(BLed,0)
-        grovepi.digitalWrite(RLed,0)
-        break # Ends program  
+        # Turns off lights ones program stops
+        lights()
+        # Ends program
+        break
     
 
 # Setting up to save the data to outputData text file
